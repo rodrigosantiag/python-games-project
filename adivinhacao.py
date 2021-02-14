996232681d3f00663ef617f10ce67f7ece0da511
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-02-14 12:56:09 -0300

    if/elif

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,13 +4,22 @@ print("********************************")
 
 numero_secreto = 42
 
-chute = input("Digite o seu numero: ")
+chute_str = input("Digite o seu numero: ")
 
-print("Você digitou", chute)
+print("Você digitou", chute_str)
 
-if numero_secreto == int(chute):
+chute = int(chute_str)
+
+acertou = numero_secreto == chute
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if acertou:
     print("Você acertou!")
 else:
-    print("Você errou!")
+    if maior:
+        print("Você errou! O seu chute foi maior que o número secreto.")
+    elif menor:
+        print("Você errou! O seu chute foi menor que o número secreto.")
 
 print("Fim do jogo")
