5247a50e1ee330f3b2261dcbda5c93431766b4f0
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-02-15 13:35:12 -0300

    Breaking and continuing `for` loop

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,9 +7,13 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite número entre 1 e 100: ")
     print("Você digitou", chute_str)
     chute = int(chute_str)
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
@@ -17,6 +21,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if acertou:
         print("Você acertou!")
+        break
     else:
         if maior:
             print("Você errou! O seu chute foi maior que o número secreto.")
