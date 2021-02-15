2890ea8c97c31b3a5188903f453f3e9a9318a7e1
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-02-15 14:55:02 -0300

    Implementing difficulty levels in our game Implementing point calculation
    Adding final message with the secret number and total points user got when user does not hit the secret number

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,7 +5,19 @@ print("Bem vindo ao jogo da adivinhação")
 print("********************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+nivel = int(input("Defina o nível:"))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -22,13 +34,16 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if acertou:
-        print("Você acertou!")
+        print(f"Você acertou e fez {pontos} pontos!")
         break
     else:
         if maior:
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif menor:
             print("Você errou! O seu chute foi menor que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos -= pontos_perdidos
+        if rodada == total_de_tentativas:
+            print(f"O número secreto era {numero_secreto}. Você fez {pontos} pontos")
 
-print(numero_secreto)
 print("Fim do jogo")
