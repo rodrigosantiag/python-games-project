c0a20a9699d52ddf95ae02d623cc5f820103df96
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-02-27 12:57:17 -0300

    Creating a file with fruits names Reading this file to choose a fruit name randomly as secret word

--- forca.py
+++ forca.py
@@ -1,9 +1,21 @@
+import random
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        palavras.append(linha.strip())
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
     letras_acertadas = ["_" for letra in palavra_secreta]
     enforcou = False
     acertou = False
